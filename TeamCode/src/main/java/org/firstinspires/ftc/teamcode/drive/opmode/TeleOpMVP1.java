9302eea59b4479b87b2120ed198d91dd5ffbaaa5
Joel Johnson <jjoel1630@gmail.com>
2022-10-29 19:06:50 -0700

    added TeleOpMVP1

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TeleOpMVP1.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.drive.opmode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.drive.DriveConstants;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+import java.util.Objects;
+
+@TeleOp(group = "drive")
+public class TeleOpMVP1 extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        waitForStart();
+
+        while (!isStopRequested()) {
+
+        }
+    }
+}
