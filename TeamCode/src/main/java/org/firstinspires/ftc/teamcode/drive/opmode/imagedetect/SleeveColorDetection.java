d9ea6c2d8bd1679c754c39f365adbd0a80b7890a
Joel Johnson <jjoel1630@gmail.com>
2022-11-21 14:18:23 -0800

    set up opencv + sample code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/imagedetect/SleeveColorDetection.java
@@ -0,0 +1,72 @@
+package org.firstinspires.ftc.teamcode.drive.opmode.imagedetect;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+
+public class SleeveColorDetection extends OpenCvPipeline {
+    // 0 for left, 1 for center, 2 for right
+    private int position = 0;
+
+    private final Scalar
+            YELLOW  = new Scalar(255, 255, 0),
+            CYAN    = new Scalar(0, 255, 255),
+            MAGENTA = new Scalar(255, 0, 255);
+
+    Point sleeve_pointA = new Point(
+            5,
+            5);
+    Point sleeve_pointB = new Point(
+            5 + 5,
+            5 + 5);
+
+    @Override
+    public Mat processFrame(Mat input) {
+        Mat areaMat = input.submat(new Rect(sleeve_pointA, sleeve_pointB));
+        Scalar sumColors = Core.sumElems(areaMat);
+
+        double minColor = Math.min(sumColors.val[0], Math.min(sumColors.val[1], sumColors.val[2]));
+
+        if (sumColors.val[0] == minColor) {
+            position = 1;
+            Imgproc.rectangle(
+                    input,
+                    sleeve_pointA,
+                    sleeve_pointB,
+                    CYAN,
+                    2
+            );
+        } else if (sumColors.val[1] == minColor) {
+            position = 2;
+            Imgproc.rectangle(
+                    input,
+                    sleeve_pointA,
+                    sleeve_pointB,
+                    MAGENTA,
+                    2
+            );
+        } else {
+            position = 0;
+            Imgproc.rectangle(
+                    input,
+                    sleeve_pointA,
+                    sleeve_pointB,
+                    YELLOW,
+                    2
+            );
+        }
+
+        areaMat.release();
+        return input;
+    }
+
+    public int getParkPosition() {
+        return position;
+    }
+}
