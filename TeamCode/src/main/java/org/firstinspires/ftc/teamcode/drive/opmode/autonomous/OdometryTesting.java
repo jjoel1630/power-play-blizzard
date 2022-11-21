fbc422b2c92f3aeaf148acc5b80f05ee004d516c
Joel Johnson <jjoel1630@gmail.com>
2022-11-21 09:22:53 -0800

    reformatted teamcode tree

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/autonomous/OdometryTesting.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.drive.opmode.autonomous;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Config
+@Autonomous(name="OdometryV1", group="Odo")
+public class OdometryTesting extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+    }
+}
