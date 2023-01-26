d26e5215027b0680ae1bf39f68491800591a0239
Joel Johnson <jjoel1630@gmail.com>
2023-01-25 22:42:50 -0800

    added odo values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/odometry/OdoNakedDt.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/odometry/OdoNakedDt.java
@@ -3,8 +3,11 @@ package org.firstinspires.ftc.teamcode.drive.opmode.odometry;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.util.Encoder;
 
 @Config
 @Autonomous(name="Odo Naked Drivetrain", group="odometry")
@@ -13,42 +16,54 @@ public class OdoNakedDt extends LinearOpMode {
 
     DcMotorEx left, right, norm;
 
-    public static double rad = 3.0;
+    public static double rad = 1.88976;
     public static double tpr = 8192;
     public static double inprev = 24;
 
     @Override
     public void runOpMode() throws InterruptedException {
         // Odometry = leftFront, leftRear, rightFront;
+        left = hardwareMap.get(DcMotorEx.class, "leftFront");
+        norm = hardwareMap.get(DcMotorEx.class, "leftRear");
+        right = hardwareMap.get(DcMotorEx.class, "rightRear");
 
         waitForStart();
 
         timer.reset();
+
+        left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        norm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         while(opModeIsActive()) {
             int lpos = left.getCurrentPosition();
             int rpos = right.getCurrentPosition();
             int npos = norm.getCurrentPosition();
 
-            double lradPos = (lpos / tpr) * (Math.PI * 2 * rad);
-            double rradPos = (lpos / tpr) * (Math.PI * 2 * rad);
-            double nradPos = (lpos / tpr) * (Math.PI * 2 * rad);
+            double circum = (Math.PI * 2 * rad);
 
-            double lsimPos = (lpos / tpr) * inprev;
-            double rsimPos = (lpos / tpr) * inprev;
-            double nsimPos = (lpos / tpr) * inprev;
+            double lradPos = Math.round(((lpos / tpr) * circum) * 100.0) / 100.0;
+            double rradPos = Math.round(((rpos / tpr) * circum) * 100.0) / 100.0;
+            double nradPos = Math.round(((npos / tpr) * circum) * 100.0) / 100.0;
 
-            telemetry.addLine("Left: " + lpos);
-            telemetry.addLine("Right: " + rpos);
-            telemetry.addLine("Norm: " + npos);
+            double lsimPos = Math.round(((lpos / tpr) * inprev) * 100.0) / 100.0;
+            double rsimPos = Math.round(((rpos / tpr) * inprev) * 100.0) / 100.0;
+            double nsimPos = Math.round(((npos / tpr) * inprev) * 100.0) / 100.0;
 
-            telemetry.addLine("Radius-based pos Left: " + lradPos);
-            telemetry.addLine("Radius-based pos Right: " + rradPos);
-            telemetry.addLine("Radius-based pos Norm: " + nradPos);
 
-            telemetry.addLine("Simulated pos Left: " + lsimPos);
-            telemetry.addLine("Simulated pos Right: " + rsimPos);
-            telemetry.addLine("Simulated pos Norm: " + nsimPos);
+            telemetry.addLine("Left (ticks): " + lpos);
+            telemetry.addLine("Right (ticks): " + rpos);
+            telemetry.addLine("Norm (ticks): " + npos);
+
+            telemetry.addLine("Radius-based pos Left (in): " + lradPos);
+            telemetry.addLine("Radius-based pos Right (in): " + rradPos);
+            telemetry.addLine("Radius-based pos Norm (in): " + nradPos);
+
+            telemetry.addLine("Simulated pos Left (in): " + lsimPos);
+            telemetry.addLine("Simulated pos Right (in): " + rsimPos);
+            telemetry.addLine("Simulated pos Normal (in): " + nsimPos);
+
+            telemetry.addLine("Circumference (in): " + circum);
 
             telemetry.update();
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/teleopm4/TeleOpAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/teleopm4/TeleOpAuton.java
@@ -1,10 +1,30 @@
 package org.firstinspires.ftc.teamcode.drive.opmode.teleopm4;
 
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDriveCancelable;
 
+@Config
+@TeleOp(name="Teleop Auton Test 1", group="Teleop Auton")
 public class TeleOpAuton extends LinearOpMode {
+    enum Mode {
+        DRIVER_CONTROL,
+        AUTOMATIC_CONTROL
+    }
+
+    Mode currentMode = Mode.DRIVER_CONTROL;
+
+    // Target A
+    Vector2d targetAVector = new Vector2d(45, 45);
+    double targetAHeading = Math.toRadians(90);
+
+    // Target B
+    Vector2d targetBVector = new Vector2d(-15, 25);
+    double targetAngle = Math.toRadians(45);
+
     @Override
     public void runOpMode() throws InterruptedException {
         SampleMecanumDriveCancelable drive = new SampleMecanumDriveCancelable(hardwareMap);
