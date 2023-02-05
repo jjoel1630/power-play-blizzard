3efd154d778f0aebcd7302c549e37bcacf374bfd
sachin@0chain.net <sachin@0chain.net>
2023-02-04 18:25:40 -0800

    tuning values before qt tuning rework

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -58,9 +58,10 @@ public class DriveConstants {
      * empirically tuned.
      */
     // Double wheel odometry
-    public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
-    public static double kA = 0.0;
-    public static double kStatic = 0.0;
+    //public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
+    public static double kV = 0.01499;
+    public static double kA = 0.002;
+    public static double kStatic = 0.073;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
