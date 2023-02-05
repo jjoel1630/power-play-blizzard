3e8f989ae34802ba2f07d571e8245b6454f7737f
sachin@0chain.net <sachin@0chain.net>
2023-02-04 18:37:10 -0800

    updated tuning values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -49,7 +49,7 @@ public class DriveConstants {
 
     public static double WHEEL_RADIUS = 1.88976; // in
     public static double GEAR_RATIO = 1.0; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 12.5; // in
+    public static double TRACK_WIDTH = 11.50; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -59,9 +59,9 @@ public class DriveConstants {
      */
     // Double wheel odometry
     //public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
-    public static double kV = 0.01499;
-    public static double kA = 0.002;
-    public static double kStatic = 0.073;
+    public static double kV = 0.01525;
+    public static double kA = 0.00179;
+    public static double kStatic = 0.07508;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -93,9 +93,9 @@ public class DriveConstants {
     // Observed maximum velocity 56 inches / sec
     // Allowance for battery: 36 inches / sec
     // 90% of the remaining...30 inches / sec
-    public static double MAX_VEL = 30.0;
-    public static double MAX_ACCEL = 30.0;
-    public static double MAX_ANG_VEL = Math.toRadians(184.02607784577722);
+    public static double MAX_VEL = 24;
+    public static double MAX_ACCEL = 24;
+    public static double MAX_ANG_VEL = Math.toRadians(237.61491982758625);
     public static double MAX_ANG_ACCEL = Math.toRadians(184.02607784577722);
 
 
